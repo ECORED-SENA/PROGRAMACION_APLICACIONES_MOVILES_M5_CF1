445ad120c1fd1de24106c21811a9246b0f13f162
lmanchego <lizethmanchego@externos-pe.cencosud.com>
2021-07-27 16:03:56 -0500

    actividad didactica

--- src/config/global.js
+++ src/config/global.js
@@ -95,12 +95,12 @@ export default {
       },
     ],
     subMenu: [
-      /* {
+      {
         nombreRuta: 'actividad',
         icono: 'far fa-question-circle',
         titulo: 'Actividad didáctica',
         desarrolloContenidos: true,
-      }, */
+      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
